20a733b12f0d698120353ecec87f20b030b473a7
Hanno Schlichting <hanno@hannosch.eu>
2012-01-25 13:37:24 +0100

    Read interval from config file

--- qdo/worker.py
+++ qdo/worker.py
@@ -11,21 +11,26 @@ class Worker(object):
         :param settings: Configuration settings
         :type settings: dict
         """
+        self.settings = settings
         self.shutdown = False
+        self.configure()
 
-    def work(self, interval=5):
+    def configure(self):
+        """Configure the worker based on the configuration settings.
+        """
+        qdo_section = self.settings.getsection('qdo-worker')
+        self.wait_interval = qdo_section.get('wait_interval', 5)
+
+    def work(self):
         """Work on jobs
 
         This is the main method of the Worker.
-
-        :param interval: Time in seconds between polling.
-        :type interval: int
         """
         while True:
             if self.shutdown:
                 break
-            print('Waiting %d seconds on next job.' % interval)
-            time.sleep(interval)
+            print('Waiting %d seconds on next job.' % self.wait_interval)
+            time.sleep(self.wait_interval)
 
 
 def run(settings):
