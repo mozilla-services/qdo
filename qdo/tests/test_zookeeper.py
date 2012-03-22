dacd0d757435220b04b958e5f4588f46d6671e77
Hanno Schlichting <hanno@hannosch.eu>
2012-03-22 15:26:21 +0100

    use new zk_connect context manager in tests

--- qdo/tests/test_zookeeper.py
+++ qdo/tests/test_zookeeper.py
@@ -24,32 +24,35 @@ class TestZookeeper(unittest.TestCase, ZKBase):
     def setUp(self):
         ZKBase._clean_zk()
 
+    def _make_one(self, hosts=u'127.0.0.1:2181,127.0.0.1:2184,'
+            '127.0.0.1:2187'):
+        return zk_connect(hosts + self.zk_root)
+
     def test_add_node_cluster_visibility(self):
         node_path = u'/node1'
-        with zk_connect(u'127.0.0.1:2181,127.0.0.1:2184,'
-                '127.0.0.1:2187' + self.zk_root) as conn1:
+        with self._make_one() as conn1:
             ZkNode(conn1, node_path)
             self.assertTrue(conn1.exists(node_path))
-        with zk_connect(u'127.0.0.1:2184' + self.zk_root) as conn2:
+        with self._make_one(u'127.0.0.1:2184') as conn2:
             self.assertTrue(conn2.exists(node_path))
-        with zk_connect(u'127.0.0.1:2187' + self.zk_root) as conn3:
+        with self._make_one(u'127.0.0.1:2187') as conn3:
             self.assertTrue(conn3.exists(node_path))
 
     def test_cluster_connection(self):
         node_path = u'/node2'
-        conn1 = self._make_zk_conn()
-        node = ZkNode(conn1, node_path)
-        node.value = 1
-        # shut down the current zk server
-        supervisor = testing.processes[u'supervisor']
-        try:
-            supervisor.stopProcess(u'zookeeper:zk1')
-            node.value = 2
-            # open a new connection and ensure the value has been set in
-            # Zookeeper
-            with zk_connect(u'127.0.0.1:2184' + self.zk_root) as conn2:
-                self.assertTrue(conn2.exists(node_path))
-                node2 = ZkNode(conn2, node_path)
-                self.assertEqual(node2.value, 2)
-        finally:
-            supervisor.startProcess(u'zookeeper:zk1')
+        with self._make_one() as conn1:
+            node = ZkNode(conn1, node_path)
+            node.value = 1
+            # shut down the current zk server
+            supervisor = testing.processes[u'supervisor']
+            try:
+                supervisor.stopProcess(u'zookeeper:zk1')
+                node.value = 2
+                # open a new connection and ensure the value has been set in
+                # Zookeeper
+                with self._make_one(u'127.0.0.1:2184') as conn2:
+                    self.assertTrue(conn2.exists(node_path))
+                    node2 = ZkNode(conn2, node_path)
+                    self.assertEqual(node2.value, 2)
+            finally:
+                supervisor.startProcess(u'zookeeper:zk1')
