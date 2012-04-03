31d9a1dcb27eda0c85f91f2bb61a1e337ed7bae1
Hanno Schlichting <hanno@hannosch.eu>
2012-04-03 16:27:35 +0200

    add a helper function to run status commands against a zk server

--- qdo/zk.py
+++ qdo/zk.py
@@ -4,6 +4,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from contextlib import contextmanager
+from socket import create_connection
 
 from zc.zk import ZooKeeper
 
@@ -17,3 +18,11 @@ def connect(hosts):
     finally:
         if conn is not None:
             conn.close()
+
+
+def sent_command(host=u'127.0.0.1', port=2181, command=b'ruok'):
+    sock = create_connection((host, port))
+    sock.sendall(command)
+    result = sock.recv(8192)
+    sock.close()
+    return [l.strip() for l in result.split('\n') if l]
