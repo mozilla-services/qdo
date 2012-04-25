18972c5a6120c603eeca754187dcff05abbf7f54
Hanno Schlichting <hanno@hannosch.eu>
2012-04-25 15:58:28 +0200

    include a copy of `mozsvc.config.SettingsDict`

--- qdo/config.py
+++ qdo/config.py
@@ -3,12 +3,85 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from mozsvc.config import SettingsDict
-
 ZOO_DEFAULT_NS = u'mozilla-qdo'
 ZOO_DEFAULT_ROOT = u'/' + ZOO_DEFAULT_NS
 ZOO_DEFAULT_HOST = u'127.0.0.1:2181,127.0.0.1:2184,127.0.0.1:2187'
 ZOO_DEFAULT_CONN = ZOO_DEFAULT_HOST + ZOO_DEFAULT_ROOT
+
+
+class SettingsDict(dict):
+    """A dict subclass with some extra helpers for dealing with app settings.
+
+    This class extends the standard dictionary interface with some extra helper
+    methods that are handy when dealing with application settings. It expects
+    the keys to be dotted setting names, where each component indicates one
+    section in the settings hierarchy. You get the following extras:
+
+        * setdefaults:  copy any unset settings from another dict
+        * getsection:   return a dict of settings for just one subsection
+    """
+
+    separator = "."
+
+    def copy(self):
+        """D.copy() -> a shallow copy of D.
+
+        This overrides the default dict.copy method to ensure that the
+        copy is also an instance of SettingsDict.
+        """
+        new_items = self.__class__()
+        for k, v in self.iteritems():
+            new_items[k] = v
+        return new_items
+
+    def getsection(self, section):
+        """Get a dict for just one sub-section of the config.
+
+        This method extracts all the keys belonging to the name section and
+        returns those values in a dict. The section name is removed from
+        each key. For example::
+
+            >>> c = SettingsDict({"a.one": 1, "a.two": 2, "b.three": 3})
+            >>> c.getsection("a")
+            {"one": 1, "two", 2}
+            >>>
+            >>> c.getsection("b")
+            {"three": 3}
+            >>>
+            >>> c.getsection("c")
+            {}
+
+        """
+        section_items = self.__class__()
+        # If the section is "" then get keys without a section.
+        if not section:
+            for key, value in self.iteritems():
+                if self.separator not in key:
+                    section_items[key] = value
+        # Otherwise, get keys prefixed with that section name.
+        else:
+            prefix = section + self.separator
+            for key, value in self.iteritems():
+                if key.startswith(prefix):
+                    section_items[key[len(prefix):]] = value
+        return section_items
+
+    def setdefaults(self, *args, **kwds):
+        """Import unset keys from another dict.
+
+        This method lets you update the dict using defaults from another
+        dict and/or using keyword arguments. It's like the standard update()
+        method except that it doesn't overwrite existing keys.
+        """
+        for arg in args:
+            if hasattr(arg, "keys"):
+                for k in arg:
+                    self.setdefault(k, arg[k])
+            else:
+                for k, v in arg:
+                    self.setdefault(k, v)
+        for k, v in kwds.iteritems():
+            self.setdefault(k, v)
 
 
 class QdoSettings(SettingsDict):
